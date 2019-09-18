11b9f1c381d6c664586ca39817ca73a52b9fc3e3
RUI LU <a10115307@gmail.com>
2019-09-18 22:02:16 +0800

    add ignore file

--- web/lucky.py
+++ web/lucky.py
@@ -6,3 +6,9 @@ import webbrowser
 import bs4
 
 print('Googling...')  ## display text while downloading Google page
+res = requests.get('http://google.com/search?q=' + ''.join(sys.argv[1:]))
+res.raise_for_status()
+
+## TODO: Retrieve top search result links
+
+## TODO: Open a browser tab for each result

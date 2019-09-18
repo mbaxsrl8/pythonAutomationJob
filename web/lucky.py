7b5a2f05ad343e5fa35e40fdc4901855c794546e
RUI LU <a10115307@gmail.com>
2019-09-18 22:30:56 +0800

    fix

--- web/lucky.py
+++ web/lucky.py
@@ -6,7 +6,7 @@ import webbrowser
 import bs4
 
 print('Googling...')  ## display text while downloading Google page
-res = requests.get('http://google.com/search?q=' + ''.join(sys.argv[1:]))
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 ## Retrieve top search result links

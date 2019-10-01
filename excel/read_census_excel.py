471c9fc256f13fe618c91f754922070dac18fb3a
RUI LU <a10115307@gmail.com>
2019-10-01 14:58:36 +0800

    midification

--- excel/read_census_excel.py
+++ excel/read_census_excel.py
@@ -11,7 +11,7 @@ countyData = {}
 
 # Fill in countyData with each county's population and tracts
 print('Reading rows')
-for row in range(2, sheet.max_row + 1):
+for row in range(2, sheet.max_row + 1)
     # Each row in the spreadsheet has data for one census tract
     state = sheet['B' + str(row)].value
     county = sheet['C' + str(row)].value
